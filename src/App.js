587bec42091cb2838b52f155fae4f1624006b7a5
Tshegofatso Mogotlane <tshegofatsom177@gmail.com>
2024-09-09 13:17:46 +0200

    added an extra component for NoNotes

--- src/App.js
+++ src/App.js
@@ -5,33 +5,18 @@ import Notes from "./components/Notes/Notes";
 import Modal from "./components/Modal/Modal";
 import { useState } from "react";
 
-const  NOTES = [
-  {
-    id:123,
-    title:"title1",
-    text: "text1",
-  },
-  {
-    id:124,
-    title:"title2",
-    text: "text2",
-  },
-  {
-    id:125,
-    title:"title3",
-    text: "text3",
-  }
- 
-]
+const  NOTES = []
+
 export const App=()=>{
-  const [notes, setNotes] = useState(NOTES)
-
+  const [notes, setNotes] = useState(NOTES);
+  
   const addNote =(note)=>{
-    notes=[
-      ...notes,
-      note
-    ]
-
+    setNotes((prevNotes)=>{
+      return[
+        ...notes,
+        note
+      ]
+    })
   }
   return (
     <div className="App">
--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -1,17 +1,24 @@
 import React from "react";
 import "./Notes.css";
 import Note from "./Note";
+import NoNotes from "../NoNotes/NoNotes";
 const Notes = (props) => {
   const { notes } = props;
   return (
     <div>
       <div className="notes">
-        {
-          notes.map((note)=><Note id={note.id} title={note.title} text={note.text}/>)
-        }
-        {/* <Note id={notes[0].id} title={notes[0].title} text={notes[0].text}/>
-        <Note id={notes[1].id} title={notes[1].title} text={notes[1].text}/> */}
-
+        {notes.length === 0 ? (
+          <NoNotes />
+        ) : (
+          notes.map((note, index) => (
+            <Note
+              key={note.index}
+              id={note.id}
+              title={note.title}
+              text={note.text}
+            />
+          ))
+        )}
       </div>
     </div>
   );
